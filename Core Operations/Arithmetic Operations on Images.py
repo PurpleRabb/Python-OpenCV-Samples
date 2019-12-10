2ebc8335671c2cd785ccdc8481899bc80c890bdd
liushuo <364725259@qq.com>
2019-12-10 15:30:08 +0800

    OpenCV: 图像的基本运算

---
+++ Core Operations/Arithmetic Operations on Images.py
@@ -0,0 +1,47 @@
+import numpy as np
+import cv2
+
+# Image Addition
+x = np.uint8([250])
+y = np.uint8([10])
+
+print(cv2.add(x, y))
+print(x + y)
+
+# Image Blending
+img1 = cv2.imread('opencv-logo2.png')
+img2 = cv2.imread('../buildings.jpg')
+
+img1 = cv2.resize(img1, (200, 200))
+img2 = cv2.resize(img2, (200, 200))
+dst = cv2.addWeighted(img1, 0.1, img2, 0.9, 0)
+# cv2.imshow('image', dst)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
+# Bitwise Operations
+img1 = cv2.imread('../buildings.jpg')
+img2 = cv2.imread('opencv-logo2.png')
+img2 = cv2.resize(img2, (100, 100))
+rows, cols, channels = img2.shape
+roi = img1[0:rows, 0:cols]
+
+# 创建mask
+img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+# cv2.imshow('img2gray', img2gray)  # logo转灰度
+ret, mask = cv2.threshold(img2gray, 150, 255, cv2.THRESH_BINARY)  # 二值化
+mask_inv = cv2.bitwise_not(mask)  # 翻转
+# cv2.imshow('mask_inv', mask_inv)
+# cv2.imshow('mask', mask)
+
+img1_bg = cv2.bitwise_and(roi, roi, mask=mask)
+# cv2.imshow('img1_bg', img1_bg)
+img2_fg = cv2.bitwise_and(img2, img2, mask=mask_inv)
+# cv2.imshow('img2_fg', img2_fg)
+
+dst = cv2.add(img1_bg, img2_fg)
+img1[0:rows, 0:cols] = dst
+cv2.imshow('res', img1)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
---
+++ Core Operations/Basic Operations on Images.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+
+img = cv2.imread('../buildings.jpg')
+px = img[100, 100]  # B G R
+print(px)
+
+# pick blue
+blue = img[100, 100, 0]
+print(blue)
+
+# modify pixel
+img[100, 100] = [255, 255, 255]
+print(img[100, 100])
+
+# better methods to access pixel
+print(img.item(100, 100, 2))  # Red
+img.itemset((100, 100, 2), 100)
+print(img.item(100, 100, 2))
+
+print(img.shape, img.size, img.dtype)
+
+_rect = img[200:300, 200:300]  # 选择200:300行、200:400列区域作为截取对象
+img[0:100, 0:100] = _rect
+# cv2.imshow('image', img)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+# Splitting and Merging Image Channels
+b, g, r = cv2.split(img)  # or b=img[:,:,0]
+img = cv2.merge((b, g, r))
+
+img[:, :, 2] = 0  # Make red 0
+BLUE = [255, 0, 0]
+constant = cv2.copyMakeBorder(img, 5, 5, 5, 5, cv2.BORDER_CONSTANT, value=BLUE)
+cv2.imshow('image', constant)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
